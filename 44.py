788c074fa30c90bc30807348b83ac1eade61c589
JINNY <103866627+yeyae@users.noreply.github.com>
2022-08-22 22:05:44 +0900

    Update 44.py

--- 44.py
+++ 44.py
@@ -2,11 +2,11 @@
 #2. global 명령어 사용하기
 #vartest_global.py
 
-a = 1
+b = 3
 def vartest():
-    global a 
-    a = a + 1
+    global b 
+    b = b + 1
 vartest()
-print(a)
+print(b)
 
 #단, 2번째 방법의 경우 외부 변수에 종속적인 함수는 좋은 함수가 아니기 때문에 가급적 43.py 파일에 있는 첫번째 방식을 택하는 것이 좋음
